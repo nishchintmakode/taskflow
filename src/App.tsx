91eee4c22d214de5baafeb7f064f080a6205bfa4
nishchintmakode <nishchintsmakode@gmail.com>
2026-02-23 12:03:39 +0000

    chore: initial Vite + shadcn setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>My WSL Setup Works!</Button>
+    </div>
+  )
+}
+
+export default App
